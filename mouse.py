5b19575b50b5fe64fb314368e96566bfbdc06318
Kurogami2134 <90238011+Kurogami2134@users.noreply.github.com>
2024-06-14 19:56:29 -0300

    Added Config File
    Added config file to set sensibility.
    Removed redundant code and added some comments.

--- mouse.py
+++ mouse.py
@@ -1,14 +1,23 @@
 import win32api
 from math import radians
+from json import load
 from struct import unpack, pack
 from filewrappers import PSPRAMFileDescriptor as PSPRam
 from time import sleep
 
 
+# CONSTANTS
 mariana = unpack('f', b'\x83\xf9\x22\x4a')[0]
+patches = {
+        "main.bin": "yawmain",
+        "fixd.bin": "fixdown",
+        "fixu.bin": "fixup",
+        "yaw_hook.bin": "yaw_h",
+        "pitch_hook.bin": "pitch_h"
+}
 
 
-def angletoyaw(i):
+def angletoyaw(i):  # Converts angle to "yaw" and "height" values
     if i >= 90:
         yaw = (180 - (i-15))*500/90
         height = 4
@@ -24,6 +33,7 @@ def angletoyaw(i):
     elif i < 90:
         yaw = max(1, (i*10))
         height = 3
+
     return max(0.1, yaw), height
 
 
@@ -56,20 +66,9 @@ class P3rdCam:
         ang = round(radians(angle)*mariana)
         self.ram.seek(self.pitch_add)
         self.ram.write(ang.to_bytes(4, "big"))
-    
-    @property
-    def height(self):
-        self.ram.seek(0x09B477A4)
-        return unpack("b", self.ram.read(1))[0]
-
-    @height.setter
-    def height(self, value):
-        self.ram.seek(0x09B477A4)
-        self.ram.write(pack("b", value))
 
 
-def install_patch(pspram):
-    patches = {"main.bin": "yawmain", "fixd.bin": "fixdown", "fixu.bin": "fixup", "yaw_hook.bin": "yaw_h", "pitch_hook.bin": "pitch_h"}
+def install_patch(pspram):  # loads assembly files into the game's ram
     with open("symfile") as file:
         symbols = { x.split(" ")[1].replace("\n", ""): int(x.split(" ")[0], 16) for x in file.readlines()[1:] if ":" not in x and " " in x}
     for file, symbol in patches.items():
@@ -79,34 +78,34 @@ def install_patch(pspram):
     return P3rdCam(pspram, symbols)
     
 
-
-
-
-
 if __name__ == "__main__":
-    CENTER = (win32api.GetSystemMetrics(0) // 2, win32api.GetSystemMetrics(1) // 2)
+    with open("config.json", "r") as file:
+        config = load(file)
 
-    last_pos = CENTER
+    CENTER = (win32api.GetSystemMetrics(0) // 2, win32api.GetSystemMetrics(1) // 2)  # Get center of the screen coords
+
     win32api.SetCursorPos(CENTER)
 
     curr_pos = win32api.GetCursorPos()
-    movement = (curr_pos[0] - last_pos[0], curr_pos[1] - last_pos[1])
-    last_pos = curr_pos
+    movement = (curr_pos[0] - CENTER[0], curr_pos[1] - CENTER[1])
 
-    sensibility = [.2, 0.20]
+    sensibility = [config["xSensibilty"], config["ySensibilty"]]
 
     pitch = 0
     yaw = 90
 
-    psp_ram = PSPRam()
+    psp_ram = PSPRam()  # hook to ppsspp memory
 
     cam = install_patch(psp_ram)
 
     while True:
         curr_pos = win32api.GetCursorPos()
         win32api.SetCursorPos(CENTER)
+
         movement = (curr_pos[0] - CENTER[0], curr_pos[1] - CENTER[1])
+
         yaw = min(140, max(0.1, yaw - (movement[1]*sensibility[1])))
+        
         pitch = pitch - (movement[0]*sensibility[0])
         if pitch < 0:
             pitch += 360
